732cf825e75be7c2ccee73f0ef8d86f3826ea177
Bucci <pgbucci@gmail.com>
2021-09-18 13:19:40 -0500

    First Commit

---
+++ android/app/src/main/java/com/smsinterceptor/MainActivity.java
@@ -0,0 +1,25 @@
+package com.smsinterceptor;
+
+import com.facebook.react.ReactActivity;
+import com.react.SmsPackage;
+import com.tkporter.sendsms.SendSMSPackage;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "SmsInterceptor";
+  }
+
+  @Override
+  public void onActivityResult(int requestCode, int resultCode, Intent data){
+    super.onActivityResult(requestCode, resultCode, data);
+    //probably some other stuff here
+    SendSMSPackage.getInstance().onActivityResult(requestCode, resultCode, data);
+  }
+  
+}
